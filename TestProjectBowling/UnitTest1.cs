46856b311c3e67bcdaca45af3f5751daacf2c855
FinnBenson <FinnBenson@CODURANCE>
2022-11-22 17:55:53 +0000

    Add project files.

---
+++ TestProjectBowling/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace TestProjectBowling
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldBe0_ZeroPinsPerBowl()
+        {
+            Game game = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                game.BowlTheBall(0);
+            }
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        [Test]
+        public void ShouldBe20_ScoreOnePinPerThrow()
+        {
+            Game game = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                game.BowlTheBall(1);
+            }
+            Assert.AreEqual(0, game.GetScore());
+        }
+    }
+}
